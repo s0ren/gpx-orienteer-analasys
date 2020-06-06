0ade248513526f2f531b8ba28b0ef57414b26e28
Søren Magnusson <soren@magnussons.dk>
2020-06-06 08:40:44 +0200

    data fram gpx file

--- README.md
+++ README.md
@@ -6,8 +6,20 @@ Stray idears of analyzing orienteering track data to evaluate training effort in
 * of-road running
 * orienteering capabilities
     * overall
-    * near orienteering
+    * near orienteering  
 Yet i'am not sure how much info i can get from tracking data
+
+## goals
+
+* identify streches with possible improvement in 
+    * runningspeed
+        * on paths/roads
+        * running in rough
+    * orinteering skills
+        * trafic light, green-yellow-red
+        * near-orienteering, time spend near the post
+        * stops during run (looking at maps or landscape)
+        * identify detours
 
 # sources
 https://en.wikipedia.org/wiki/GPS_Exchange_Format
--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@ import gpxpy.gpx
 # Parsing an existing file:
 # -------------------------
 
-gpx_file = open('activity_4931694757.gpx', 'r')
+gpx_file = open('data/activity_4931694757.gpx', 'r')
 
 gpx = gpxpy.parse(gpx_file)
 
@@ -15,8 +15,8 @@ for track in gpx.tracks:
         for point in segment.points:
             print('Point at ({0},{1}) -> {2}'.format(point.latitude, point.longitude, point.elevation))
 
-# for waypoint in gpx.waypoints:
-#     print('waypoint {0} -> ({1},{2})'.format(waypoint.name, waypoint.latitude, waypoint.longitude))
+for waypoint in gpx.waypoints:
+    print('waypoint {0} -> ({1},{2})'.format(waypoint.name, waypoint.latitude, waypoint.longitude))
 
 # for route in gpx.routes:
 #     print('Route:')
